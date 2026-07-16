22b93ba1d106353b6710771686475fbf22beadeb
Dean <deansylvester7@gmail.com>
2026-07-16 15:31:41 -0500

    update backend

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from pinecone import Pinecone
 from dotenv import load_dotenv
 from flask_cors import CORS
 import os
-
+print("APP VERSION 7.16.26 WITH EMPLOYEE HANDBOOK")
 load_dotenv()
 
 SECTION_STARTS = [
